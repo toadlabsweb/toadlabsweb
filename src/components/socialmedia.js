2439e96550ef2ff2564babdc761a34831ca635c1
toadlabsweb <93641736+toadlabsweb@users.noreply.github.com>
2021-11-16 18:28:23 +0530

    Update socialmedia.js

--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -6,16 +6,16 @@ import { Link } from 'gatsby'
  
         return ( 
             <>  
-            <Link data-bs-toggle="tooltip" title="Facebook" to="https://www.facebook.com/toadlabs">
+            <Link data-bs-toggle="tooltip" title="Facebook" to="https://www.facebook.com/toadlabs" target="_blank">
                 <IoLogoFacebook className="footer-icons mx-3" />
             </Link>
-            <Link data-bs-toggle="tooltip" title="Youtube" to="https://www.youtube.com/channel/UCwSzfZtkIHZ3s5V3dUldaXg">
+            <Link data-bs-toggle="tooltip" title="Youtube" to="https://www.youtube.com/channel/UCOpRzSwbHON9NGsmvr2yjoA" target="_blank">
                 <IoLogoYoutube className="footer-icons mx-3" />
             </Link>
-            <Link data-bs-toggle="tooltip" title="Instagram" to="https://www.instagram.com/toadlabs_technology_inc/">
+            <Link data-bs-toggle="tooltip" title="Instagram" to="https://www.instagram.com/toadlabs_technology_inc/" target="_blank">
                 <IoLogoInstagram className="footer-icons mx-3" />
             </Link>
-            <Link data-bs-toggle="tooltip" title="Linkedin" to="https://www.linkedin.com/in/toadlabs-technology-inc-376593226/">
+            <Link data-bs-toggle="tooltip" title="Linkedin" to="https://www.linkedin.com/in/toadlabs-technology-inc-376593226/" target="_blank">
                 <IoLogoLinkedin className="footer-icons mx-3" />
             </Link> 
         </> 
