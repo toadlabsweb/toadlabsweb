9809ebcbd2e67f2dd367306b0512a39a18f2b768
toadlabsweb <93641736+toadlabsweb@users.noreply.github.com>
2021-11-21 11:51:56 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,12 +48,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sass",
     {
-      resolve: "gatsby-plugin-react-leaflet",
-      options: {
-        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
-      }
-    }, 
-    {
       resolve:"gatsby-plugin-google-fonts",
       options: {
         fonts: [
