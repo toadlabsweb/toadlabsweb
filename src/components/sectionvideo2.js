f02df7b4dd8af40e4fba3d7f05781b71a1fabff5
toadlabsweb <93641736+toadlabsweb@users.noreply.github.com>
2022-01-14 18:45:20 +0530

    Update sectionvideo2.js

--- src/components/sectionvideo2.js
+++ src/components/sectionvideo2.js
@@ -7,7 +7,7 @@ const SectionVideo2 = ({ image, title, subtitle, buttonText }) => {
   
  
   return (
-        <div className="section-video">
+        <div className="section-video ratio ratio-16x9">
             <video playsinline controls preload muted>
               <source src={TLvideo} type="video/mp4"/>
             </video> 
