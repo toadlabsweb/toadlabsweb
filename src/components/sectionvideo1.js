56a5bc404d04fe9d5efba53162eaacd0cda589ea
toadlabsweb <93641736+toadlabsweb@users.noreply.github.com>
2022-01-14 18:45:01 +0530

    Update sectionvideo1.js

--- src/components/sectionvideo1.js
+++ src/components/sectionvideo1.js
@@ -7,7 +7,7 @@ const SectionVideo1 = ({ image, title, subtitle, buttonText }) => {
   
  
   return (
-        <div className="section-video">
+        <div className="section-video ratio ratio-16x9">
             <video playsinline controls preload muted>
               <source src={Adsintheair} type="video/mp4"/>
             </video>    
